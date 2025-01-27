88fb80a4a042a8c01e57edfe4325e901f7204ee1
TheMr1 <patrykaleksandersroka@gmail.com>
2025-01-27 19:17:55 +0100

    Improve Geometry.py error handling

--- Geometry.py
+++ Geometry.py
@@ -1,46 +1,51 @@
 import math
+from MainLibrary.MainPackage import BasicFunctions
+
+list_of_operations = ["Area", "Perimeter"]
 
 print('Available operations: ')
-print('"Area", "Perimeter"')
+print(list_of_operations)
 
-operation = input("Chose the desired operation: ")
+operation = BasicFunctions.ask_for_string("Choose the desired operation: ", list_of_operations)
+
+list_of_shapes = ["Triangle", "Rectangle", "Circle"]
 
 print('Available shapes: ')
-print('"Triangle", "Rectangle", "Circle"')
+print(list_of_shapes)
 
-shape = input("Chose the desired operation: ")
+shape = BasicFunctions.ask_for_string("Choose the desired shape: ", list_of_shapes)
 
 result = 0
 
 if operation == "Area":
     if shape == "Triangle":
-        num1 = float(input("Input a side length of the triangle: "))
-        num2 = float(input("Input the height projected onto this side: "))
+        num1 = BasicFunctions.ask_for_float("Input a side length of the triangle: ", False)
+        num2 = BasicFunctions.ask_for_float("Input the height projected onto this side: ", False)
 
         result = num1 * num2 * 1/2
     elif shape == "Rectangle":
-        num1 = float(input("Input the first side length: "))
-        num2 = float(input("Input the second side length: "))
+        num1 = BasicFunctions.ask_for_float("Input the first side length: ", False)
+        num2 = BasicFunctions.ask_for_float("Input the second side length: ", False)
 
         result = num1 * num2
     elif shape == "Circle":
-        num1 = float(input("Input the length of the radius: "))
+        num1 = BasicFunctions.ask_for_float("Input the length of the radius: ", False)
 
         result = num1 * num1 * math.pi
 elif operation == "Perimeter":
     if shape == "Triangle":
-        num1 = float(input("Input the length of the first side : "))
-        num2 = float(input("Input the length of the second side: "))
-        num3 = float(input("Input the length of the third side: "))
+        num1 = BasicFunctions.ask_for_float("Input the first side length: ", False)
+        num2 = BasicFunctions.ask_for_float("Input the second side length: ", False)
+        num3 = BasicFunctions.ask_for_float("Input the third side length: ", False)
 
         result = num1 + num2 + num3
     elif shape == "Rectangle":
-        num1 = float(input("Input the length of the first side : "))
-        num2 = float(input("Input the length of the second side: "))
+        num1 = BasicFunctions.ask_for_float("Input the first side length: ", False)
+        num2 = BasicFunctions.ask_for_float("Input the second side length: ", False)
 
         result = num1 * 2 + num2 * 2
     elif shape == "Circle":
-        num1 = float(input("Input the length of the radius: "))
+        num1 = BasicFunctions.ask_for_float("Input the length of the radius: ", False)
 
         result = 2 * num1 * math.pi
 
