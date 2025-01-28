49574ad8b495e7c74dc065b555f4bfabaa0da93d
TheMr1 <patrykaleksandersroka@gmail.com>
2025-01-28 21:00:27 +0100

    Add Inputs.txt and integrated it with Main.py

--- Main.py
+++ Main.py
@@ -1,50 +1,97 @@
 import math
 from MainLibrary.MainPackage import BasicFunctions
 
+from_file1 = False
+
+num1 = False
+num2 = False
+operation = False
+
 list_of_operations = ["+","-","*","/","^"]
 
-print('Available operations: ')
-print(list_of_operations)
-operation = BasicFunctions.ask_for_string("Choose the desired operation: ", list_of_operations)
+try:
+    file = open("Inputs.txt", 'r')
+    line1 = file.readline()
+    operation = line1[0: line1.__len__() - 1]
 
-def attempt_to_calculate():
+    if not list_of_operations.__contains__(operation):
+        operation = 1/0
+
+    line2 = file.readline()
+    num1 = float(line2[0: line2.__len__() - 1])
+
+    line3 = file.readline()
+    num2 = float(line3[0: line3.__len__()])
+
+    from_file1 = True
+except FileNotFoundError:
+    pass
+except IndexError:
+    pass
+except ValueError:
+    pass
+except ZeroDivisionError:
+    pass
+
+if not from_file1:
+    print('Available operations: ')
+    print(list_of_operations)
+    operation = BasicFunctions.ask_for_string("Choose the desired operation: ", list_of_operations)
+
+def attempt_to_calculate(from_file, num1, num2):
     try:
         if operation == '+':
-            num1 = BasicFunctions.ask_for_float("Choose the first number: ", False)
-            num2 = BasicFunctions.ask_for_float("Choose the second number: ", False)
+            if not from_file:
+                num1 = BasicFunctions.ask_for_float("Choose the first number: ", False)
+                num2 = BasicFunctions.ask_for_float("Choose the second number: ", False)
 
             result1 = num1 + num2
         elif operation == '-':
-            num1 = BasicFunctions.ask_for_float("Choose the first number: ", False)
-            num2 = BasicFunctions.ask_for_float("Choose the second number: ", False)
+            if not from_file:
+                num1 = BasicFunctions.ask_for_float("Choose the first number: ", False)
+                num2 = BasicFunctions.ask_for_float("Choose the second number: ", False)
 
             result1 = num1 - num2
         elif operation == '*':
-            num1 = BasicFunctions.ask_for_float("Choose the first number: ", False)
-            num2 = BasicFunctions.ask_for_float("Choose the second number: ", False)
+            if not from_file:
+                num1 = BasicFunctions.ask_for_float("Choose the first number: ", False)
+                num2 = BasicFunctions.ask_for_float("Choose the second number: ", False)
 
             result1 = num1 * num2
         elif operation == '/':
-            num1 = BasicFunctions.ask_for_float("Choose the first number: ", False)
-            num2 = BasicFunctions.ask_for_float("Choose the second number: ", False)
+            if not from_file:
+                num1 = BasicFunctions.ask_for_float("Choose the first number: ", False)
+                num2 = BasicFunctions.ask_for_float("Choose the second number: ", False)
 
             result1 = num1 / num2
         elif operation == '^':
-            num11 = BasicFunctions.ask_for_float("Choose the base: ", False)
-            num22 = BasicFunctions.ask_for_float("Choose the exponent: ", False)
+            if not from_file:
+                num1 = BasicFunctions.ask_for_float("Choose the base: ", False)
+                num2 = BasicFunctions.ask_for_float("Choose the exponent: ", False)
 
-            result1 = math.pow(num11, num22)
+            result1 = math.pow(num1, num2)
         return result1
     except ValueError:
         print('Cannot calculate operation, try again.')
-        return attempt_to_calculate()
+        return attempt_to_calculate(from_file, num1, num2)
     except OverflowError:
         print('Cannot calculate operation, try again.')
-        return attempt_to_calculate()
+        return attempt_to_calculate(from_file, num1, num2)
     except ZeroDivisionError:
         print('Cannot calculate operation, try again.')
-        return attempt_to_calculate()
+        return attempt_to_calculate(from_file, num1, num2)
+    except UnboundLocalError:
+        print('Cannot calculate operation.')
+        #return attempt_to_calculate(from_file, num1, num2)
+    except RecursionError:
+        print('Cannot calculate operation.')
+        #return attempt_to_calculate(from_file, num1, num2)
 
-result = attempt_to_calculate()
+result = attempt_to_calculate(from_file1, num1, num2)
 
-print('The result is: ' + str(result))
+if from_file1:
+    print('The result (file numbers) is: ' + str(result))
+else:
+    print('The result (input numbers) is: ' + str(result))
+
+
