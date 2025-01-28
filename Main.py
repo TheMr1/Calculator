07ef42b20f62686c8bc10da9f3aa57c03bee5171
TheMr1 <patrykaleksandersroka@gmail.com>
2025-01-28 19:52:03 +0100

    Further improve Main.py error handling

--- Main.py
+++ Main.py
@@ -7,49 +7,44 @@ print('Available operations: ')
 print(list_of_operations)
 operation = BasicFunctions.ask_for_string("Choose the desired operation: ", list_of_operations)
 
-result = 0
+def attempt_to_calculate():
+    try:
+        if operation == '+':
+            num1 = BasicFunctions.ask_for_float("Choose the first number: ", False)
+            num2 = BasicFunctions.ask_for_float("Choose the second number: ", False)
 
-if operation == '+':
-    num1 = BasicFunctions.ask_for_float("Choose the first number: ", False)
-    num2 = BasicFunctions.ask_for_float("Choose the second number: ", False)
+            result1 = num1 + num2
+        elif operation == '-':
+            num1 = BasicFunctions.ask_for_float("Choose the first number: ", False)
+            num2 = BasicFunctions.ask_for_float("Choose the second number: ", False)
 
-    result = num1 + num2
-elif operation == '-':
-    num1 = BasicFunctions.ask_for_float("Choose the first number: ", False)
-    num2 = BasicFunctions.ask_for_float("Choose the second number: ", False)
+            result1 = num1 - num2
+        elif operation == '*':
+            num1 = BasicFunctions.ask_for_float("Choose the first number: ", False)
+            num2 = BasicFunctions.ask_for_float("Choose the second number: ", False)
 
-    result = num1 - num2
-elif operation == '*':
-    num1 = BasicFunctions.ask_for_float("Choose the first number: ", False)
-    num2 = BasicFunctions.ask_for_float("Choose the second number: ", False)
+            result1 = num1 * num2
+        elif operation == '/':
+            num1 = BasicFunctions.ask_for_float("Choose the first number: ", False)
+            num2 = BasicFunctions.ask_for_float("Choose the second number: ", False)
 
-    result = num1 * num2
-elif operation == '/':
-    num1 = BasicFunctions.ask_for_float("Choose the first number: ", False)
+            result1 = num1 / num2
+        elif operation == '^':
+            num11 = BasicFunctions.ask_for_float("Choose the base: ", False)
+            num22 = BasicFunctions.ask_for_float("Choose the exponent: ", False)
 
-    def ask_for_num2():
-        num2_1 = BasicFunctions.ask_for_float("Choose the second number: ", False)
-        if num2_1 == 0:
-            print('Cannot divide by zero, try again.')
-            return ask_for_num2()
-        else:
-            return num2_1
-
-    num2 = ask_for_num2()
-
-    result = num1 / num2
-elif operation == '^':
-    def attempt_to_calculate_exponentiation():
-        num11 = BasicFunctions.ask_for_float("Choose the base: ", False)
-        num22 = BasicFunctions.ask_for_float("Choose the exponent: ", False)
-
-        try:
             result1 = math.pow(num11, num22)
-            return result1
-        except ValueError:
-            print('Cannot perform exponentiation, try again.')
-            return attempt_to_calculate_exponentiation()
+        return result1
+    except ValueError:
+        print('Cannot calculate operation, try again.')
+        return attempt_to_calculate()
+    except OverflowError:
+        print('Cannot calculate operation, try again.')
+        return attempt_to_calculate()
+    except ZeroDivisionError:
+        print('Cannot calculate operation, try again.')
+        return attempt_to_calculate()
 
-    result = attempt_to_calculate_exponentiation()
+result = attempt_to_calculate()
 
 print('The result is: ' + str(result))
